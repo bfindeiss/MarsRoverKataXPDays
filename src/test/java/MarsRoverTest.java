5d5cdbd400310e13a028376826afc5f36ba76339
bfindeiss <bernhard.findeiss@googlemail.com>
2012-11-27 14:10:47 +0100

    Cleared Imports

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,13 +1,6 @@
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.isA;
 import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.hasItems;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.hamcrest.Matcher;
-import org.junit.Before;
 import org.junit.Test;
 
 public class MarsRoverTest {
